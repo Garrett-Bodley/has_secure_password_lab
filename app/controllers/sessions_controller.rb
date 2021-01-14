d948970c2cfe11287c776a1342674830b0bf40cb
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-01-13 23:40:04 -0500

    finished lab

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    @user = User.find_by(name: user_params[:name])
+    if @user.authenticate(user_params[:password])
+      session[:user_id] = @user.id
+      redirect_to @user
+    else
+      redirect_to '/'
+    end
+  end
+
+  def destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
+  end
+
+end
